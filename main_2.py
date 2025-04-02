d90ce83e07c1f9d15bd30a3b05af1fd763d7ed86
Oleksandr190378 <147439788+Oleksandr190378@users.noreply.github.com>
2025-04-02 09:59:15 +0300

    Update main_2.py  max_terms - 2000

--- main_2.py
+++ main_2.py
@@ -258,7 +258,7 @@ def main():
                         help='Розмір чанка для запитів до бази даних')
     parser.add_argument('--update_chunk', type=int, default=app_config['update_chunk'],
                         help='Розмір чанка для оновлення бази даних')
-    parser.add_argument('--max_terms', type=int, default=100000,
+    parser.add_argument('--max_terms', type=int, default=2000,
                         help='Максимальна кількість пошукових термінів для обробки за один раз')
     parser.add_argument('--parallel', action='store_true',
                         help='Використовувати паралельну обробку даних')
